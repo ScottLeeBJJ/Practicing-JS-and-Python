654adc33792463289f10175383a71b930726aaf1
Scott Lee <56131751+ScottLeeBJJ@users.noreply.github.com>
2023-01-21 16:41:05 -0600

    setting up Turtle Screen

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import turtle
 import time
 
 # window or screen
-
+screen = turtle.Screen()
+screen.setup(800,800)
+screen.title("Connect 4 Game with Python!")
+turtle.speed(0)
+turtle.hideturtle()
+screen.tracer(0,0)
+score = turtle.Turtle()
+score.up()
+score.hideturtle()
 
 # connect 4 board
 
