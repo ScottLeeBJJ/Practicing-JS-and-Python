7cc620ce98d481cdb6b1ef9ee032ad8ad4f0c606
Scott Lee <56131751+ScottLeeBJJ@users.noreply.github.com>
2023-01-21 16:10:38 -0600

    initial code push

---
+++ main.py
@@ -0,0 +1,24 @@
+# connect 4 game
+
+import turtle
+import time
+
+# window or screen
+
+
+# connect 4 board
+
+
+# player pieces
+
+
+# AI pieces
+
+
+# you win
+
+
+# you lose
+
+
+# close the game
