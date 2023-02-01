6f4ef3a5c631dc9782ed83fb7a79e546b917f90b
Scott Lee <56131751+ScottLeeBJJ@users.noreply.github.com>
2023-02-01 16:57:55 -0600

    continuing with turtle

--- main.py
+++ main.py
@@ -22,6 +22,19 @@ STARTY = -450*ROWS/COLS
 WIDTH = 2*STARTX
 HEIGHT = -2*STARTY
 
+def draw_rectangle(x,y,w,h,color):
+    turtle.up()
+    turtle.goto(x,y)
+    turtle.set(0)
+    turtle.down()
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+    turtle.fd(w)
+    turtle.left(90)
+    turtle.fd(h)
+    turtle.left(90)
+    
+
 # player pieces
 
 
